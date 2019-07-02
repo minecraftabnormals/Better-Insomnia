27a21954716bd4353035d5e4da3cb6b8441a554e
The_Chipmunk <astpasshandler@gmail.com>
2019-07-01 17:47:01 -0700

    Add files via upload

---
+++ src/main/java/com/teamabnormals/better_insomnia/core/lists/BIBlockList.java
@@ -0,0 +1,24 @@
+package com.betterinsomnia.lists;
+
+import net.minecraft.block.Block;
+
+public class BIBlockList {
+	
+	public static Block WHITE_BEDROLL;
+	public static Block ORANGE_BEDROLL;
+	public static Block MAGENTA_BEDROLL;
+	public static Block LIGHT_BLUE_BEDROLL;
+	public static Block YELLOW_BEDROLL;
+	public static Block LIME_BEDROLL;
+	public static Block PINK_BEDROLL;
+	public static Block GRAY_BEDROLL;
+	public static Block LIGHT_GRAY_BEDROLL;
+	public static Block CYAN_BEDROLL;
+	public static Block PURPLE_BEDROLL;
+	public static Block BLUE_BEDROLL;
+	public static Block BROWN_BEDROLL;
+	public static Block GREEN_BEDROLL;
+	public static Block RED_BEDROLL;
+	public static Block BLACK_BEDROLL;
+	
+}
